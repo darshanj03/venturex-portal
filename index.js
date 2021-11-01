ffd60bc08858451f9b355ebc59e53928fc4f877c
CHANDRA DARSHAN J <darshanjc03@gmail.com>
2021-11-01 20:05:22 +0530

    created

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error', ()=>console.log("Error in connection to db"));
+db.once('open', ()=>console.log("connected"));
+
+app.post("/sign_up", (req,res) => {
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var message = req.body.message;
+
+    var data = {
+        "name": name,
+        "email": email,
+        "phno": phno,
+        "message": message
+    }
+    db.collection('users').insertOne(data, (err, collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record inserted successfully");
+    });
+
+    return res.redirect('signup_success.html')
+})
+
+app.get("/", (req, res) => {
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000);
+
+console.log("Listening on port 3000");
